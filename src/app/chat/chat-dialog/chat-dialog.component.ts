79fca0df503e56982005b388445f8d1e52107257
Divyanshu Saini <Divyanshu.Saini@lntinfotech.com>
2017-12-13 11:48:29 +0530

    minor changes

--- src/app/chat/chat-dialog/chat-dialog.component.ts
+++ src/app/chat/chat-dialog/chat-dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-chat-dialog',
+  selector: 'chat-dialog',
   templateUrl: './chat-dialog.component.html',
   styleUrls: ['./chat-dialog.component.css']
 })
