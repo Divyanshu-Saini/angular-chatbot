43794002e11927d2ff1c2a1572c76ea67455c453
Divyanshu Saini <Divyanshu.Saini@lntinfotech.com>
2017-12-18 16:31:21 +0530

    some changes

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { ChatDialogComponent } from './chat-dialog/chat-dialog.component';
 import { ChatService } from './chat.service';
 import { SpeechService } from './speech.service';
+import { VirtualScrollModule } from 'angular2-virtual-scroll';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    VirtualScrollModule
   ],
   declarations: [ChatDialogComponent],
   // exporting chatdialogcomponent
