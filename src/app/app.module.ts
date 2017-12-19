4d59c66a016549b23e5cb99f0677df8ac5e93942
Bhavik Chandora <Bhavik.Chandora@lntinfotech.com>
2017-12-19 11:54:33 +0530

    scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { ChatModule } from './chat/chat.module';
 
