bc9bb3455075f5caa5c89290dc0dfa1b32bf6642
Divyanshu Saini <Divyanshu.Saini@lntinfotech.com>
2017-12-29 16:18:39 +0530

    environment changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  dialogflow: {
+    angularBot_Bhavik: '734d75343a474dec864092239fb3b383',
+    angularBot_Divyanshu: '84cd0e73a8644e57bebf061e25327059'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
 
   dialogflow: {
     angularBot_Bhavik: '734d75343a474dec864092239fb3b383',
